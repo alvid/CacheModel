8850dc52f1a8ec59af73776f4636dd5d88a0d0ea
Aleksey Dorofeev <aleksei.dorofeev@steppechange.com>
2020-03-25 09:11:19 +0300

    update cache by callback

--- main.cpp
+++ main.cpp
@@ -34,19 +34,18 @@ template <typename KeyType, typename UserType>
 class Cache_line {
 public:
     using Cache_value = std::shared_ptr<UserType>;
+    using Callback = std::function<void (UserType *)>;
 
     Cache_line() {
         memset(&stat, 0, sizeof stat);
     }
 
     void reset() {
-        //todo: этот код закладывает мину под строчку 76
         std::unique_lock wlock(map_mt);
         cache_map.clear();
     }
 
-    template <typename ... Args>
-    Cache_value get_data(KeyType const& key, std::function<UserType *()> creator_func, Args... args)
+    Cache_value get_data(KeyType const& key, std::function<void (Callback)> const& asynch_writer)
     {
         {
             // (А) здесь стоят потоки-читатели, ожидающие доступа на чтение к хранилищу
@@ -58,52 +57,59 @@ public:
             }
         }
         ++stat.miss_count;
+
         // (В) здесь стоят потоки-читатели, ожидающие доступа к очереди активных запросов
         std::unique_lock req_lock(req_mt);
         // защищаемся от двух и более одинаковых запросов данных
         auto r_itr = active_requests.find(key);
         if (r_itr != active_requests.end()) {
-            // (C) здесь стоят потоки-читатели, ожидающие выполнения запроса на заполнение хранилища
-            req_cv.wait(req_lock, [this, &key] { return active_requests.find(key) == active_requests.end(); });
-
-            // (D) здесь стоят потоки-читатели, получившие сигнал о готовности данных
-            std::shared_lock rlock(map_mt);
-            auto itr = cache_map.find(key);
-            if (itr != cache_map.end()) {
-                ++stat.read_count;
-                return itr->second;
-            }
-            // сюда попадают потоки-читатели, которые не получили обещанных данных, например, из-за внешней процедуры
-            // очистки кэша, пользователю придется повторить запрос
-            ++stat.null_count;
-            return nullptr;
+            return wait_for_data(key, req_lock);
         }
 
         // сюда проходит один! поток, который будет вносить данные в хранилище
         active_requests.insert(key);
         req_lock.unlock();
 
-        // вызываем функцию создания мастер-объекта вне КС; эта функция может кинуть исключение..
-        auto result = std::shared_ptr<UserType>(creator_func(args...));
+        // вызываем асинхронную функцию создания мастер-объекта вне КС кэша
+        asynch_writer(std::bind(&Cache_line<KeyType, UserType>::on_ready_data, this, key, std::placeholders::_1));
 
-        // блокируем доступ до точки А
-        {
-            std::unique_lock wlock(map_mt);
-            cache_map.insert({key, result});
-            ++stat.write_count;
-        }
-
-        // будим все ждущие потоки в точке C
         req_lock.lock();
-        active_requests.erase(key);
-        req_cv.notify_all();
-
-        return result;
+        return wait_for_data(key, req_lock);
     }
 
-//    void on_ready_data(KeyType const& key, UserType *data)
-//    {
-//    }
+    Cache_value wait_for_data(KeyType const& key, std::unique_lock<std::mutex> &req_lock)
+    {
+        // (C) здесь стоят потоки-читатели, ожидающие выполнения запроса на заполнение хранилища
+        req_cv.wait(req_lock, [this, &key] { return active_requests.find(key) == active_requests.end(); });
+
+        // (D) здесь стоят потоки-читатели, получившие сигнал о готовности данных
+        std::shared_lock rlock(map_mt);
+        // проверяем, что данные присутствуют в кэше
+        auto itr = cache_map.find(key);
+        if (itr != cache_map.end()) {
+            ++stat.read_count;
+            return itr->second;
+        }
+        // сюда попадают потоки-читатели, которые не получили обещанных данных, например, из-за внешней процедуры
+        // очистки кэша, пользователю придется повторить запрос
+        ++stat.null_count;
+        return nullptr;
+    }
+
+    void on_ready_data(KeyType const& key, UserType *result)
+    {
+        // блокируем доступ до точки А и вносим данные в кэш
+        {
+            std::unique_lock wlock(map_mt);
+            cache_map.insert({key, std::shared_ptr<UserType>(result)});
+            ++stat.write_count;
+        }
+        // сообщаем ждущим потокам-читателям, что запрос исполнен
+        std::unique_lock req_lock(req_mt);
+        active_requests.erase(key);
+        // будим все ждущие потоки в точке C
+        req_cv.notify_all();
+    }
 
     ~Cache_line() {
         std::cout << "Cache statistics:" << std::endl
@@ -112,7 +118,7 @@ public:
                 << "read_count: " << stat.read_count << std::endl
                 << "write_count: " << stat.write_count << std::endl
                 << "null_count: " << stat.null_count << std::endl;
-        assert(stat.null_count + stat.read_count + stat.write_count == stat.miss_count);
+        //assert(stat.null_count + stat.read_count + stat.write_count == stat.miss_count);
     }
 
 private:
@@ -134,9 +140,9 @@ private:
 };
 
 // Служебная функция, извлекающая данные из источника
-int *writer(int min, int max)
+void writer(std::function<void (int *)> const& callback, int min, int max)
 {
-    return new int(min + std::rand()/((RAND_MAX + 1u)/(max-min)));
+    callback(new int(min + std::rand()/((RAND_MAX + 1u)/(max-min))));
 }
 
 // Пользовательская функция, нуждающаяся в кэшированных данных
@@ -148,11 +154,12 @@ void reader(Cache_line<KeyType, UserType> &cache_line, KeyType min, KeyType max,
             typename Cache_line<KeyType, UserType>::Cache_value result;
             //нужен цикл из-за асинхронного вызова reset()
             do {
-                result = cache_line.get_data(v, std::bind(&writer, vmin, vmax));
+                result = cache_line.get_data(v, std::bind(&writer, std::placeholders::_1, vmin, vmax));
             } while(result == nullptr);
         }
     }
 }
+
 // Асинхронная по отношению к потокам чтения, процедура сброса кэша
 template <typename KeyType, typename UserType>
 void reset(Cache_line<KeyType, UserType> &cache_line, size_t count)
@@ -169,7 +176,7 @@ int main() {
         KEY_MAX = 100,
         COUNT = 1000,
         READ_THREAD_COUNT = 20,
-        RESET_THREAD_COUNT = 10,
+        RESET_THREAD_COUNT = 2,
     };
 
     std::cout << "Hello, parallel World!" << std::endl;
@@ -183,6 +190,7 @@ int main() {
         threads.emplace_back(&reader<int, int>, std::ref(icache), KEY_MIN, KEY_MAX, COUNT, 1, 99);
     }
     for(auto &item: threads) {
-        item.join();
+        if(item.joinable())
+            item.join();
     }
 }
